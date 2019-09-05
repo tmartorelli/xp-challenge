4379311b25ad6210778c6d7b522f8a9eb1022eeb
tmartorelli <tiziano.martorelli@gmail.com>
2019-09-05 23:20:25 +0200

    Added vpc module

---
+++ modules/vpc/vars.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+    description = "the name of the environment this vpc is deployed into"
+    type = "string"
+    default = "production"
+}
+
+variable "region" {
+    description = "the region to deploy to"
+    type = "string"
+    default = "eu-west-1"
+}
---
+++ modules/vpc/vpc.tf
@@ -0,0 +1,147 @@
+resource "aws_vpc" "default" {
+  cidr_block = "192.168.0.0/19"
+  instance_tenancy = "default"
+}
+
+resource "aws_internet_gateway" "gateway" {
+  vpc_id ="${aws_vpc.default.id}"
+  tags {
+      key = "Name"
+      value = "${var.environment}"
+      propagate_at_launch = true
+  }
+}
+
+resource "aws_eip" "nat_ip_1" {
+  vpc = true
+}
+
+resource "aws_eip" "nat_ip_2" {
+  vpc = true
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = "${aws_vpc.default.id}"
+  availability_zone = "${var.region}a"
+  cidr_block = "192.168.4.0/23"
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id = "${aws_vpc.default.id}"
+  availability_zone = "${var.region}b"
+  cidr_block = "192.168.6.0/23"
+}
+
+resource "aws_subnet" "dmz_subnet_1" {
+  vpc_id = "${aws_vpc.default.id}"
+  availability_zone = "${var.region}a"
+  cidr_block = "192.168.0.0/23"
+}
+
+resource "aws_subnet" "dmz_subnet_2" {
+  vpc_id = "${aws_vpc.default.id}"
+  availability_zone = "${var.region}a"
+  cidr_block = "192.168.2.0/23"
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = "${aws_vpc.default.id}"
+  availability_zone = "${var.region}a"
+  cidr_block = "192.168.8.0/23"
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id = "${aws_vpc.default.id}"
+  availability_zone = "${var.region}b"
+  cidr_block = "192.168.10.0/23"
+}
+
+resource "aws_nat_gateway" "ngw_1" {
+  allocation_id = "${aws_eip.nat_ip_1.id}"
+  subnet_id = "${aws_subnet.public_subnet_1.id}"
+}
+
+resource "aws_nat_gateway" "ngw_2" {
+  allocation_id = "${aws_eip.nat_ip_2.id}"
+  subnet_id = "${aws_subnet.public_subnet_2.id}"
+}
+
+
+resource "aws_route_table" "vpc_route" {
+  vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gateway.id}"
+    }
+}
+
+resource "aws_route_table" "dmz_route_table" {
+  vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gateway.id}"
+    }
+}
+
+resource "aws_route_table_association" "dmz_route_association_1" {
+  subnet_id      = "${aws_subnet.dmz_subnet_1.id}"
+  route_table_id = "${aws_route_table.dmz_route_table.id}"
+}
+
+resource "aws_route_table_association" "dmz_route_association_2" {
+  subnet_id      = "${aws_subnet.dmz_subnet_2.id}"
+  route_table_id = "${aws_route_table.dmz_route_table.id}"
+}
+
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gateway.id}"
+
+    }
+}
+
+resource "aws_route_table_association" "public_route_association_1" {
+  subnet_id      = "${aws_subnet.public_subnet_1.id}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+}
+
+resource "aws_route_table_association" "public_route_association_2" {
+  subnet_id      = "${aws_subnet.public_subnet_2.id}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+}
+
+resource "aws_route_table" "private_route_table_1" {
+  vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_nat_gateway.ngw_1.id}"
+
+    }
+}
+
+resource "aws_route_table_association" "private_route_association1" {
+  subnet_id      = "${aws_subnet.private_subnet_1.id}"
+  route_table_id = "${aws_route_table.private_route_table_1.id}"
+}
+
+resource "aws_route_table" "private_route_table_2" {
+  vpc_id = "${aws_vpc.default.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_nat_gateway.ngw_2.id}"
+
+    }
+}
+
+resource "aws_route_table_association" "private_route_association2" {
+  subnet_id      = "${aws_subnet.private_subnet_2.id}"
+  route_table_id = "${aws_route_table.private_route_table_2.id}"
+}
